e0d3d088653435a33a27c14c45794e50eddac47f
Paul Ebreo <pebreo@imagescape.com>
2016-02-02 02:48:13 -0600

    use Chrome instead of firefox

--- test_closerlook.py
+++ test_closerlook.py
@@ -8,7 +8,7 @@ class BaseTestCase(unittest.TestCase):
     
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Firefox()
+        cls.driver = webdriver.Chrome()
         cls.driver.implicitly_wait(5)
         super(BaseTestCase, cls).setUpClass()
 
